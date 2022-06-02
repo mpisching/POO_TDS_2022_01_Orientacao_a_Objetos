6dcefc6d32c0ddbbdc10b1a7a5879554583ecc93
Marcos André Pisching <mpisching@gmail.com>
2022-06-02 15:30:51 -0300

    Aula do dia 02/06/2022 - atividade
    Atividade sobre Interfaces - Tipos de Porta

---
+++ Estudo3/SmartHome/src/domain/Correr.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author mpisc
+ */
+public class Correr extends Porta {
+    public Correr(Localizacao local) {
+        super(local);
+    }
+}
